3f19f2a9e405e2de6ea80a22a0c0d51f233772f1
duphqua <mr.latygolets@mail.ru>
2025-05-14 11:40:55 +0300

    Create qwer.java

---
+++ qwer.java
@@ -0,0 +1,9 @@
+public class qwer {
+    public static void main(String[] args) {
+        String surname = "Латыголец";         
+        String subject = "ТРПО"; 
+
+        System.out.println("Surname: " + surname);
+        System.out.println("Subject: " + subject);
+    }
+}
